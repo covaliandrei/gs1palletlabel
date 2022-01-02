4c9e9067dcc7d40e866f51a5803427d4b49ae788
oopsadmin <covali.andrei.box@gmail.com>
2022-01-02 20:07:07 +0200

    Added Suppliers and Destination models

--- gs1palletlabel/main/models.py
+++ gs1palletlabel/main/models.py
@@ -7,3 +7,18 @@ class Suppliers(models.Model):
 
     def __str__(self):
         return self.supplier
+
+    class Meta:
+        verbose_name ='Supplier'
+        verbose_name_plural = 'Suppliers'
+
+class Destinations(models.Model):
+    destination = models.CharField('destination', max_length=150)
+    destination_name = models.TextField('DestinationName')
+
+    def __str__(self):
+        return self.destination
+
+    class Meta:
+        verbose_name ='Destination'
+        verbose_name_plural = 'Destinations'
